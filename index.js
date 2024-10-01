8373592cc789159c41434aae4f8cf61704680d8a
Alex Surprenant <102190687+Soup098@users.noreply.github.com>
2024-09-30 22:48:15 -0600

    update

--- index.js
+++ index.js
@@ -4,34 +4,60 @@
 const searchInput = document.getElementById("search-input")
 const searchBtn = document.getElementById("search-btn")
 const movieCardContainer = document.getElementById("movie-card-container")
+movieSearches = 0
+movieList = []
+watchList = []
 
 searchBtn.addEventListener("click", () => {
     fetch(`http://www.omdbapi.com/?t=${searchInput.value}&apikey=44f84d4e`)
         .then(response => response.json())
         .then(movie => {
             console.log(movie)
-            movieCardContainer.innerHTML += `
+            const newMovie = {
+                poster: movie.Poster,
+                title: movie.Title,
+                imdbRating: movie.imdbRating,
+                runtime: movie.Runtime,
+                genre: movie.Genre,
+                plot: movie.Plot,
+                ID: movie.imdbID,
+            }
+            movieList.push(newMovie)
+            console.log(movieList)
+            renderMovie()
+            movieSearches++
+        })
+})
+
+// function addToWatchList(){
+//     document.getElementById(${movieList})
+// }
+
+function renderMovie(){
+
+    movieCardContainer.innerHTML += `
                 <div class="movie-card" id="movie-card">
-                    <img class="movie-poster" src="${movie.Poster}"/>
+                    <img class="movie-poster" src="${movieList[movieSearches].poster}"/>
                     <div class="movie-info">
                         <div class="movie-title-rating">
-                            <h2 class="movie-title">${movie.Title}</h2>
-                            <h4>⭐${movie.imdbRating}</h4>
+                            <h2 class="movie-title">${movieList[movieSearches].title}</h2>
+                            <h4>⭐${movieList[movieSearches].imdbRating}</h4>
                         </div>
                         <div class="runtime-genre-watchlist">
-                            <p>${movie.Runtime}</p>
-                            <p>${movie.Genre}</p>
-                            <button class="add-to-watchlist">+ Watchlist</button>
+                            <p>${movieList[movieSearches].runtime}</p>
+                            <p>${movieList[movieSearches].genre}</p>
+                            <button id="${movieList[movieSearches].ID}">+ Watchlist</button>
                         </div>
-                        <p>${movie.Plot}</p>
+                        <p>${movieList[movieSearches].plot}</p>
                     </div>
                     <br>
                 </div>
                 <hr>
-
             `
-        })
-})
+                
+}
+
+
 
 /*TODO
 change the JS so that the movie info is saved to an object called movieDetails, then pull the info
