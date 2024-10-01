89ba0377d60b8523b6e796445f8afa9c19bc15f0
Alex Surprenant <102190687+Soup098@users.noreply.github.com>
2024-09-30 20:49:30 -0600

    update

--- index.js
+++ index.js
@@ -3,11 +3,37 @@
 
 const searchInput = document.getElementById("search-input")
 const searchBtn = document.getElementById("search-btn")
+const movieCardContainer = document.getElementById("movie-card-container")
 
 searchBtn.addEventListener("click", () => {
     fetch(`http://www.omdbapi.com/?t=${searchInput.value}&apikey=44f84d4e`)
         .then(response => response.json())
-        .then(data => {
-            console.log(data)
+        .then(movie => {
+            console.log(movie)
+            movieCardContainer.innerHTML += `
+                <div class="movie-card" id="movie-card">
+                    <img class="movie-poster" src="${movie.Poster}"/>
+                    <div class="movie-info">
+                        <div class="movie-title-rating">
+                            <h2 class="movie-title">${movie.Title}</h2>
+                            <h4>⭐${movie.imdbRating}</h4>
+                        </div>
+                        <div class="runtime-genre-watchlist">
+                            <p>${movie.Runtime}</p>
+                            <p>${movie.Genre}</p>
+                            <button class="add-to-watchlist">+ Watchlist</button>
+                        </div>
+                        <p>${movie.Plot}</p>
+                    </div>
+                    <br>
+                </div>
+
+            `
         })
 })
+
+/*TODO
+change the JS so that the movie info is saved to an object called movieDetails, then pull the info
+from movieDetails to the movieCardContainer.innerHTML using a loop. This will make it easier to delete a movie card from the list.
+
+*/
