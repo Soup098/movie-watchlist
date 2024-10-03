b7c6c31b3dd82417b3a273ecac131c996d18113d
Alex Surprenant <102190687+Soup098@users.noreply.github.com>
2024-10-03 12:11:42 -0600

    update

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 //44f84d4e
 //http://www.omdbapi.com/?apikey=[yourkey]&
 
+
+// assign variables for objects in the DOM, search counter, and movieList and watchList arrays. 
 const searchInput = document.getElementById("search-input")
 const searchBtn = document.getElementById("search-btn")
 const movieCardContainer = document.getElementById("movie-card-container")
@@ -8,12 +10,13 @@ movieSearches = 0
 movieList = []
 watchList = []
 
+//event listener for the search button that calls the API to receive movie data.
 searchBtn.addEventListener("click", () => {
     fetch(`http://www.omdbapi.com/?t=${searchInput.value}&apikey=44f84d4e`)
         .then(response => response.json())
         .then(movie => {
             console.log(movie)
-            const newMovie = {
+            const newMovie = { // create a new movie object to add to the movieList array
                 poster: movie.Poster,
                 title: movie.Title,
                 imdbRating: movie.imdbRating,
@@ -24,17 +27,13 @@ searchBtn.addEventListener("click", () => {
             }
             movieList.push(newMovie)
             console.log(movieList)
-            renderMovie()
-            movieSearches++
+            renderMovie() // use the renderMovie() function to render the searched movie to the DOM
+            movieSearches++ //increment the movie searches count by 1 (Used later in the code)
         })
 })
 
-// function addToWatchList(){
-//     document.getElementById(${movieList})
-// }
-
+//fuction to render the moview to the DOM. data is added to the button element within the innerHTML in order to extract data from the contianer and add to the watchlist
 function renderMovie(){
-
     movieCardContainer.innerHTML += `
                 <div class="movie-card" id="movie-card">
                     <img class="movie-poster" src="${movieList[movieSearches].poster}"/>
@@ -46,16 +45,22 @@ function renderMovie(){
                         <div class="runtime-genre-watchlist">
                             <p>${movieList[movieSearches].runtime}</p>
                             <p>${movieList[movieSearches].genre}</p>
-                            <button id="${movieList[movieSearches].ID}">+ Watchlist</button>
+                            <button class="watchlist-btn movie-poster="${[movieSearches].poster}" movie-title="${movieList[movieSearches].title}" 
+                            movie-imdbRating="${movieList[movieSearches].imdbRating}" movie-runtime="${movieList[movieSearches].runtime}" 
+                            movie-genre="${movieList[movieSearches].genre}" movie-plot="${movieList[movieSearches].plot}">+ Watchlist</button>
                         </div>
                         <p>${movieList[movieSearches].plot}</p>
                     </div>
                     <br>
                 </div>
                 <hr>
-            `
-                
+            `  
 }
+
+// function to add movie to the watchlist using the add movie button
+// function addToWatchList(){
+//     document.getElementById(${movieList})
+// }
 
 
 
